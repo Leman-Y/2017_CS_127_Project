6156b8341243a9c83e6d06ae28cb1c60fe765c8e
Jason <Jason.Chen53@myhunter.cuny.edu>
2017-12-07 15:22:13 -0500

    updated Scrabble

--- Scrabble.py
+++ Scrabble.py
@@ -12,14 +12,26 @@ What we need to do:
     -All players draw seven new letters and place them on their racks.
     -If two players tie for first, repeat
 -Add function that allows the user to pull up the rules.txt file when needed
--Merriam-Webster's Official Scrabble® Players Dictionary, Fourth Edition (OSPD4) Thats the dictionary scrabble uses
--
-
+-Each word will go through a dictionary library to see if it is part of the english dictionary.
 '''
-
 
 players = {}
 num_p = 0
+letter_score = {"#" : "0",
+                "aeioulnrst":"1", 
+                "dg":"2", 
+                "bcmp":"3", 
+                "fhvwy":"4",
+                "k":"5",
+                "jx":"8",
+                "qz":"10"}
+basket_of_letters = ["a", 9, "b", 2, "c", 2, "d", 4, "e", 12
+                    "f", 2, "g", 3, "h", 2, "i", 9, "j", 1,
+                    "k", 1, "l", 4, "m", 2, "n", 6, "o", 8,
+                    "p", 2, "q", 1, "r", 6, "s", 4, "t", 6, 
+                    "u", 4, "v", 2, "w", 2, "x", 1, "y", 2, 
+                    "z", 1, "#", 2 ]
+
 
 def ask_player():
     '''
