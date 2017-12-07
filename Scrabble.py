41a3b1b314079caad0a984c86b48aa7494171500
Solepride <leman.yan80@myhunter.cuny.edu>
2017-12-07 00:20:33 -0500

    Added text

--- Scrabble.py
+++ Scrabble.py
@@ -1,3 +1,23 @@
+'''
+Scrabble Project Outline
+----------------------
+
+What we did:
+
+-Ask player: Scrabble is a 2-4 player game so when you input 1 then we should repeat the question until they give us 2-4!
+
+What we need to do:
+
+-Draw for first play. The player with the letter closest to "A" plays first. A blank tile beats any letter. Return the letters to the pool and remix.
+    -All players draw seven new letters and place them on their racks.
+    -If two players tie for first, repeat
+-Add function that allows the user to pull up the rules.txt file when needed
+-Merriam-Webster's Official Scrabble® Players Dictionary, Fourth Edition (OSPD4) Thats the dictionary scrabble uses
+-
+
+'''
+
+
 players = {}
 num_p = 0
 
