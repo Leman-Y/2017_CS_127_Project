fa7be24709a98e2c62ad756bb5ba02b1a8ecf093
Jason <Jason.Chen53@myhunter.cuny.edu>
2017-12-07 21:45:55 -0500

    editted the scrabble board

--- Scrabble.py
+++ Scrabble.py
@@ -21,7 +21,7 @@ What we need to do:
 players = {}
 num_p = 0
 
-alpha = "abcdefghijklmnopqrstuvwxyz"
+alpha = "abcdefghijklmnopqrstuvwxyz#"
 
 letter_score = {"#" : "0",
                 "aeioulnrst":"1", 
@@ -64,41 +64,54 @@ def randomtile():
     -use random.choice
     -
     '''
-     
-
+    
 TRIPLE_WORD_SCORE = ((0,0), (7, 0), (14,0), (0, 7), (14, 7), (0, 14), (7, 14), (14,14))
 DOUBLE_WORD_SCORE = ((1,1), (2,2), (3,3), (4,4), (1, 13), (2, 12), (3, 11), (4, 10), (13, 1), (12, 2), (11, 3), (10, 4), (13,13), (12, 12), (11,11), (10,10), (7,7))
 TRIPLE_LETTER_SCORE = ((1,5), (1, 9), (5,1), (5,5), (5,9), (5,13), (9,1), (9,5), (9,9), (9,13), (13, 5), (13,9))
 DOUBLE_LETTER_SCORE = ((0, 3), (0,11), (2,6), (2,8), (3,0), (3,7), (3,14), (6,2), (6,6), (6,8), (6,12), (7,3), (7,11), (8,2), (8,6), (8,8), (8, 12), (11,0), (11,7), (11,14), (12,6), (12,8), (14, 3), (14, 11))
 
+
 def make_scrabble_board():
     '''
     Makes scrabble board
+    12/7 9:45 pm UPDATE made it os board has numbers and letters on side
     '''
     board=[]
-    for i in range(15):
+    
+    for i in range(16):
         line=[]
-        for i in range(15):
-            line.append('_')
+        if (i != 0):
+            if (i < 10):
+                hello = str(i) + " "
+                line.append(hello)
+            else:
+                line.append(str(i))
+        for j in range(16):
+            if (i ==0 and j ==0):
+                line.append('_ ')
+            elif (i == 0 and  j < 17):
+                line.append(alpha[j-1])
+            elif (j < 15):
+                line.append('_')
         board.append(line)
 
     for r,c in TRIPLE_WORD_SCORE:
-        board[r][c] = 'T'
+        board[r+1][c+1] = 'T'
 
     for r,c in DOUBLE_WORD_SCORE:
-        board[r][c] = 'D'
+        board[r+1][c+1] = 'D'
 
     for r,c in TRIPLE_LETTER_SCORE:
-        board[r][c]='t'
+        board[r+1][c+1]='t'
 
     for r,c in DOUBLE_LETTER_SCORE:
-        board[r][c] = 'd'
+        board[r+1][c+1] = 'd'
     return board
 
 
 def print_board(b):  #Organizes the lines so it looks like a board
     for line in b:
-        print ( ' '.join(line))
+        print (' '.join(line))
 
 print_board(make_scrabble_board())
 
